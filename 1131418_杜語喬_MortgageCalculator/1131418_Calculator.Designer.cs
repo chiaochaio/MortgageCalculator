eeb81217347db00df02e7f711db1371e3906febf
chiaochiao <s1131418@mail.yzu.edu.tw>
2026-04-01 22:00:38 +0800

    加入專案檔案。

---
+++ 1131418_杜語喬_MortgageCalculator/1131418_Calculator.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace _1131418_杜語喬_MortgageCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Left = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tbGrace = new System.Windows.Forms.TextBox();
+            this.tbTerm = new System.Windows.Forms.TextBox();
+            this.tbRate = new System.Windows.Forms.TextBox();
+            this.tbDown2 = new System.Windows.Forms.TextBox();
+            this.tbDown = new System.Windows.Forms.TextBox();
+            this.tbTotal = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Left2 = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.right = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Left.SuspendLayout();
+            this.Left2.SuspendLayout();
+            this.right.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Left
+            // 
+            this.Left.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Left.Controls.Add(this.label11);
+            this.Left.Controls.Add(this.tbGrace);
+            this.Left.Controls.Add(this.tbTerm);
+            this.Left.Controls.Add(this.tbRate);
+            this.Left.Controls.Add(this.tbDown2);
+            this.Left.Controls.Add(this.tbDown);
+            this.Left.Controls.Add(this.tbTotal);
+            this.Left.Controls.Add(this.label10);
+            this.Left.Controls.Add(this.label9);
+            this.Left.Controls.Add(this.label8);
+            this.Left.Controls.Add(this.label7);
+            this.Left.Controls.Add(this.label6);
+            this.Left.Controls.Add(this.label5);
+            this.Left.Controls.Add(this.label1);
+            this.Left.Controls.Add(this.Left2);
+            this.Left.Controls.Add(this.label12);
+            this.Left.Controls.Add(this.label3);
+            this.Left.Controls.Add(this.label13);
+            this.Left.Controls.Add(this.label14);
+            this.Left.Controls.Add(this.label15);
+            this.Left.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Left.Location = new System.Drawing.Point(12, 49);
+            this.Left.Name = "Left";
+            this.Left.Size = new System.Drawing.Size(603, 467);
+            this.Left.TabIndex = 0;
+            this.Left.TabStop = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label11.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label11.Location = new System.Drawing.Point(150, 246);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(122, 22);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "(兩者擇一輸入)";
+            // 
+            // tbGrace
+            // 
+            this.tbGrace.BackColor = System.Drawing.SystemColors.Menu;
+            this.tbGrace.Location = new System.Drawing.Point(151, 419);
+            this.tbGrace.Name = "tbGrace";
+            this.tbGrace.Size = new System.Drawing.Size(65, 38);
+            this.tbGrace.TabIndex = 14;
+            this.tbGrace.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // tbTerm
+            // 
+            this.tbTerm.BackColor = System.Drawing.SystemColors.Menu;
+            this.tbTerm.Location = new System.Drawing.Point(151, 353);
+            this.tbTerm.Name = "tbTerm";
+            this.tbTerm.Size = new System.Drawing.Size(188, 38);
+            this.tbTerm.TabIndex = 13;
+            // 
+            // tbRate
+            // 
+            this.tbRate.BackColor = System.Drawing.SystemColors.Menu;
+            this.tbRate.Location = new System.Drawing.Point(151, 290);
+            this.tbRate.Name = "tbRate";
+            this.tbRate.Size = new System.Drawing.Size(188, 38);
+            this.tbRate.TabIndex = 12;
+            // 
+            // tbDown2
+            // 
+            this.tbDown2.BackColor = System.Drawing.SystemColors.Menu;
+            this.tbDown2.Location = new System.Drawing.Point(151, 205);
+            this.tbDown2.Name = "tbDown2";
+            this.tbDown2.Size = new System.Drawing.Size(188, 38);
+            this.tbDown2.TabIndex = 11;
+            this.tbDown2.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // tbDown
+            // 
+            this.tbDown.BackColor = System.Drawing.SystemColors.Menu;
+            this.tbDown.Location = new System.Drawing.Point(151, 157);
+            this.tbDown.Name = "tbDown";
+            this.tbDown.Size = new System.Drawing.Size(188, 38);
+            this.tbDown.TabIndex = 10;
+            // 
+            // tbTotal
+            // 
+            this.tbTotal.BackColor = System.Drawing.SystemColors.Menu;
+            this.tbTotal.Location = new System.Drawing.Point(151, 87);
+            this.tbTotal.Name = "tbTotal";
+            this.tbTotal.Size = new System.Drawing.Size(188, 38);
+            this.tbTotal.TabIndex = 9;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(52, 422);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(93, 29);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "寬限期 :";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(29, 356);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(116, 29);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "貸款年限 :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 293);
+            this.label8.Name = "label8";
+            this.label8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label8.Size = new System.Drawing.Size(139, 29);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "貸款年利率 :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(75, 208);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 29);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "金額 :";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(139, 29);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "自備款比例 :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 29);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "房屋總價 :";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(15, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "輸入貸款詳情";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Left2
+            // 
+            this.Left2.Controls.Add(this.btnClear);
+            this.Left2.Controls.Add(this.btnEnter);
+            this.Left2.Location = new System.Drawing.Point(364, 0);
+            this.Left2.Name = "Left2";
+            this.Left2.Size = new System.Drawing.Size(239, 467);
+            this.Left2.TabIndex = 0;
+            this.Left2.TabStop = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(16, 414);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(213, 43);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "全部清除 (Ctrl+R)";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(16, 360);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(213, 43);
+            this.btnEnter.TabIndex = 0;
+            this.btnEnter.Text = "試算 (Enter key)";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("微軟正黑體 Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label12.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label12.Location = new System.Drawing.Point(-16, 115);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(383, 29);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "_____________________________________";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.Location = new System.Drawing.Point(-5, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(383, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "_____________________________________";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("微軟正黑體 Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label13.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label13.Location = new System.Drawing.Point(-5, 246);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(383, 29);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "_____________________________________";
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("微軟正黑體 Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label14.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label14.Location = new System.Drawing.Point(-5, 315);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(383, 29);
+            this.label14.TabIndex = 18;
+            this.label14.Text = "_____________________________________";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("微軟正黑體 Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label15.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label15.Location = new System.Drawing.Point(-5, 382);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(383, 29);
+            this.label15.TabIndex = 19;
+            this.label15.Text = "_____________________________________";
+            // 
+            // right
+            // 
+            this.right.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.right.Controls.Add(this.label2);
+            this.right.Controls.Add(this.label4);
+            this.right.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.right.Location = new System.Drawing.Point(635, 49);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(527, 467);
+            this.right.TabIndex = 1;
+            this.right.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(15, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 42);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "計算結果";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label4.Location = new System.Drawing.Point(6, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(333, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "________________________________";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1177, 528);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.Left);
+            this.Name = "Form1";
+            this.Text = "SmartCalculator";
+            this.Left.ResumeLayout(false);
+            this.Left.PerformLayout();
+            this.Left2.ResumeLayout(false);
+            this.right.ResumeLayout(false);
+            this.right.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Left;
+        private System.Windows.Forms.GroupBox Left2;
+        private System.Windows.Forms.GroupBox right;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tbGrace;
+        private System.Windows.Forms.TextBox tbTerm;
+        private System.Windows.Forms.TextBox tbRate;
+        private System.Windows.Forms.TextBox tbDown2;
+        private System.Windows.Forms.TextBox tbDown;
+        private System.Windows.Forms.TextBox tbTotal;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ 1131418_杜語喬_MortgageCalculator/1131418_Calculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1131418_杜語喬_MortgageCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
